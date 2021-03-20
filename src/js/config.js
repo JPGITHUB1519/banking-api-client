84a13a38cf4ad837f43cbc584a33c3bc38292271
Jean Urena <juanpedro1519@gmail.com>
2021-03-20 12:45:58 -0400

    adding production api url and identifying development and production

--- src/js/config.js
+++ src/js/config.js
@@ -1,1 +1,14 @@
-export const API_URL = 'http://localhost:8090/php-banking-api-test-gpcuaw/project/api';
+let API_URL;
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  // development
+  API_URL = 'http://localhost:8090/php-banking-api-test-gpcuaw/api';
+  console.log('development');
+} else {
+  // production
+  API_URL = 'https://php-banking-api.herokuapp.com/api';
+  console.log('production');
+}
+
+export { API_URL };
+
